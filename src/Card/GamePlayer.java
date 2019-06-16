336098cd4427aab6d11869d510d9a22deed6916c
escober <escober@sheridancollege.ca>
2019-06-16 03:55:45 -0400

    CardGame

--- src/Card/GamePlayer.java
+++ src/Card/GamePlayer.java
@@ -3,18 +3,19 @@ package Card;
 
 public class GamePlayer
 {
-
+   
    public static void main (String[] args)
    {
-
+      
       CardHand ch = new CardHand();
-
+      
       ch.generateHand();
       for (Card c : ch.cards) {
          System.out.println(c.getRanks() + " of " + c.getColor());
-
+         
       }
-
+      System.out.println("End of the program");
+      
    }
-
+   
 }
